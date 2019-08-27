c828506af2aa065e50460663bed8195888828033
Antoine Rigitano <antoine@rigitano.com>
2019-08-27 12:00:25 +0200

    Trackers ID Circle

---
+++ Assets/Scripts/SceneScripts/TrackerInitialization.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerInitialization : MonoBehaviour
+{
+    public ViveTracker[] trackers;
+
+    void Awake()
+    {
+        trackers = GameObject.FindObjectsOfType<ViveTracker>();
+    }
+
+    public void Initialization(string newText)
+    {
+        int number = int.Parse(newText);
+
+        if(number <= trackers.Length)
+        {
+            for(int i = 0; i<number; i++)
+            {
+                trackers[i].gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            Debug.Log("Number too big");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TrackersComponents/TrackerCircle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 2D circle displayed at the feet of the player.
+/// </summary>
+public class TrackerCircle : MonoBehaviour
+{
+    /// <summary>
+    /// Image of the circle
+    /// </summary>
+    [SerializeField]
+    private Image _circle;
+    /// <summary>
+    /// Layer of the ground
+    /// </summary>
+    [SerializeField]
+    private LayerMask _ground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _circle.color = new Color(Random.Range(0, 255) / 255f, Random.Range(0, 255) / 255f, Random.Range(0, 255) / 255f);
+
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+        //Circle will position itself on the ground.
+        if (Physics.Raycast(transform.position, forward, out hit, 50, _ground))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, hit.collider.transform.position.z);
+        }
+    }
+}
