e2911cf937aacb29b527994beda10f00579b9cf5
Antoine Rigitano <antoine@rigitano.com>
2019-08-27 17:39:01 +0200

    Musical chair script

---
+++ Assets/Scripts/SceneScripts/Examples/MusicalChairs.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// A simple musical chairs game.
+/// </summary>
+public class MusicalChairs : MonoBehaviour
+{
+    /// <summary>
+    /// The number of chairs to be spawned.
+    /// </summary>
+    private int _nbChairs = 8;
+    /// <summary>
+    /// Prefab of a chair.
+    /// </summary>
+    [SerializeField]
+    private GameObject _chairPrefab;
+    /// <summary>
+    /// Radius of the circle of chairs.
+    /// </summary>
+    [SerializeField]
+    private float _radius = 5f;
+    /// <summary>
+    /// Layer of the ground.
+    /// </summary>
+    [SerializeField]
+    private LayerMask _ground;
+    /// <summary>
+    /// Duration of a round in seconds.
+    /// </summary>
+    [SerializeField]
+    private int _roundDuration = 60;
+    /// <summary>
+    /// The players.
+    /// </summary>
+    private List<GameObject> _players;
+    /// <summary>
+    /// Text displaying current status of the game.
+    /// </summary>
+    [SerializeField]
+    private Text _status;
+    /// <summary>
+    /// Waiting delay between each round.
+    /// </summary>
+    [SerializeField]
+    private int _waitingTime = 10;
+
+    /// <summary>
+    /// Launching the game.
+    /// </summary>
+    public void GameStart()
+    {
+        _players = TrackersManager.instance.trackers;
+        _nbChairs = _players.Count - 1;
+        StartCoroutine(Rounds());
+    }
+
+    /// <summary>
+    /// Manages the rounds.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Rounds()
+    {
+        for (int i = 0; i < _nbChairs; i++)
+        {
+            ChairsInstantiating();
+
+            for (int j = _waitingTime; j >= 0; j--)
+            {
+                _status.text = "Round starts in " + j;
+                yield return new WaitForSeconds(1f);
+            }
+
+            for (int j = _roundDuration; j >= 0; j--)
+            {
+                _status.text = j + " seconds remaining";
+                yield return new WaitForSeconds(1f);
+            }
+
+            _nbChairs--;
+            _status.text = "End of round";
+        }
+    }
+
+    /// <summary>
+    /// Spawns the right number of chairs in a circle.
+    /// </summary>
+    private void ChairsInstantiating()
+    {
+        for (int i = 0; i < _nbChairs; i++)
+        {
+            float angle = i * Mathf.PI * 2f / _nbChairs;
+            Vector3 position = new Vector3(Mathf.Cos(angle) * _radius, Mathf.Sin(angle) * _radius, transform.position.y);
+            GameObject chair = Instantiate(_chairPrefab, position, Quaternion.identity);
+            StickGround(chair);
+        }
+    }
+
+    /// <summary>
+    /// Object will position itself on the ground.
+    /// </summary>
+    private void StickGround(GameObject mesh)
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast(transform.position, forward, out hit, 50, _ground))
+        {
+            mesh.transform.position = new Vector3(mesh.transform.position.x, mesh.transform.position.y, hit.collider.transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneScripts/Examples/TrackersPosition.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Displays the position of the trackers
+/// </summary>
+public class TrackersPosition : MonoBehaviour
+{
+    /// <summary>
+    /// Prefab of the position text for a single tracker
+    /// </summary>
+    [SerializeField]
+    private GameObject _textPrefab;
+    /// <summary>
+    /// List of all the position texts
+    /// </summary>
+    private List<GameObject> _positionTexts = new List<GameObject>();
+       
+    /// <summary>
+    /// Creates a position text for a new tracker
+    /// </summary>
+    public void NewPositionText()
+    {
+        GameObject positionText = Instantiate(_textPrefab, transform.position, transform.rotation);
+        positionText.name = "Vive Tracker" + TrackersManager.instance.trackers.Count.ToString();
+        positionText.transform.SetParent(this.transform);
+        
+        _positionTexts.Add(positionText);
+    }
+
+    /// <summary>
+    /// Removes the position text of a disconnected tracker
+    /// </summary>
+    public void RemovePositiontext()
+    {
+        for(int i=0; i<_positionTexts.Count; i++)
+        {
+            if(_positionTexts[i].name != "TrackersManager.instance.trackers[i].name")
+            {
+                Destroy(_positionTexts[i].gameObject);
+                _positionTexts.RemoveAt(i);
+                break;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Updates the displayed positions of all the trackers
+        for (int i = 0; i < _positionTexts.Count; i++)
+        {
+            _positionTexts[i].GetComponent<Text>().text = TrackersManager.instance.trackers[i].name + "\nx=" 
+                + TrackersManager.instance.trackers[i].transform.position.x.ToString() + " " 
+                + "y=" + TrackersManager.instance.trackers[i].transform.position.y.ToString() + " " 
+                + "z=" + TrackersManager.instance.trackers[i].transform.position.z.ToString();
+        }
+    }
+}
