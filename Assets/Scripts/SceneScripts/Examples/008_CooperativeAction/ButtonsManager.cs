59ba84b5c62192666561236554d3312294e283ca
Antoine Rigitano <antoine@rigitano.com>
2019-09-09 22:51:29 +0200

    Cooperation scene

---
+++ Assets/Scripts/SceneScripts/Examples/008_CooperativeAction/ButtonsManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _door;
+    private CooperativeButton[] _buttons;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _buttons = GameObject.FindObjectsOfType<CooperativeButton>();
+    }
+
+    public void OpenDoor()
+    {
+        if()
+    }
+}
