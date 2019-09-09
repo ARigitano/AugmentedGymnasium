96fae1bc8ea2e20b2f8a4312e22137704926668a
Antoine Rigitano <antoine@rigitano.com>
2019-09-09 14:24:24 +0200

    Score scene complete

---
+++ Assets/Scripts/SceneScripts/Examples/007_Score/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script of a coin for the scoring scene.
+/// </summary>
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "PlayerArea")
+        {
+            other.GetComponent<TrackerCircle>().score++;
+            Destroy(gameObject);
+        }
+    }
+}
